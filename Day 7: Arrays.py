762e8a9cd307e66344b55ea9530dd2e1cee0fd9e
YASHasvi SHUkla <72089033+YASHasvi-SHUkla@users.noreply.github.com>
2021-09-22 05:17:25 +0530

    Create Day 7: Arrays.py
    Objective
    Today, we will learn about the Array data structure.
    
    Sample Input
    
    4
    1 4 3 2
    Sample Output
    
    2 3 4 1

---
+++ Day 7: Arrays.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+draken = list(reversed(arr))
+for i in draken:
+    print(i,end=" ")
